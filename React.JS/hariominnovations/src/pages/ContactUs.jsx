66861d7a77d4e35def412e1fbee70705bcba73b6
AnkitaTorane <ankitatorane5395@gmail.com>
2024-12-26 23:51:39 +0530

    HariOm Project

---
+++ React.JS/hariominnovations/src/pages/ContactUs.jsx
@@ -0,0 +1,13 @@
+import MainNavBar from "../commponents/MainNavBar"
+
+const ContactUs = () => {
+
+    return(
+        <div>
+            <MainNavBar />
+
+        <h1> Contact Us page</h1>
+        </div>
+    )
+}
+export default ContactUs
