def720e76f129da8389ff524cc1aa7862d76db4b
AnkitaTorane <ankitatorane5395@gmail.com>
2025-01-05 00:41:14 +0530

    Footer

--- React.JS/hariominnovations/src/pages/ContactUs.jsx
+++ React.JS/hariominnovations/src/pages/ContactUs.jsx
@@ -1,5 +1,11 @@
 import MainNavBar from "../commponents/MainNavBar"
-
+import logo from "../Images/footer.png"
+import Location from "../Images/Location-icon.png"
+import message from "../Images/Message-icon.png"
+import call from "../Images/Call-icon.png"
+import facebook from "../Images/facebook-icon.png"
+import twiter from "../Images/twiter-icon.png"
+import instagram from "../Images/instagram-icon.png"
 const ContactUs = () => {
 
     return (
@@ -27,17 +33,36 @@ const ContactUs = () => {
             <footer>
                 <div className="Container">
                     <div className="row py-5">
-                        <div className="col-md-3">
-                            <h6>Hari om </h6>
+                        <div className="col-md-4">
+                            <img src={logo} />
+                            <p className="my-5" >
+                                Innovation is the driving force behind progress,
+                                transforming ideas into impactful solutions that address modern challenges.
+                                Innovation is the driving force behind progress, transforming ideas into impactful solutions that address.
+                            </p>
+                            <ul>
+                                <li>
+                                    <img src={Location} className="footer-icons" />1234 Elm Street, Springfield, IL 62704,
+                                    New York
+                                </li>
+                                <li>
+                                    <img src={message} className="footer-icons" />supporthariom@gmail.com
+                                </li>
+                                <li>
+                                    <img src={call} className="footer-icons" />(212) 555-7890
+                                </li>
+                            </ul>
                         </div>
                         <div className="col-md-3">
-                            <h6>Company </h6>   
+                            <h6>Company </h6>
                             <ul>
                                 <li> About us</li>
                                 <li>Careers</li>
                                 <li> Case Studies</li>
                                 <li> Testimonials</li>
                             </ul>
+                            <br />
+                            <br />
                             <h6>Industry </h6>
                             <ul>
                                 <li>Education & Learning</li>
@@ -70,7 +95,7 @@ const ContactUs = () => {
                                 <li>QA and Testing</li>
                             </ul>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-2">
 
                             <h6>Technology </h6>
                             <ul>
@@ -91,11 +116,19 @@ const ContactUs = () => {
                             </ul>
                         </div>
                     </div>
+                    <hr />
+                    <div className="pb-3 copy-right-div">
+                        <span>Copyright: © 2024 By Hariom Innovations</span>
+                        <div>
+                            <img src={facebook} className="social-icon-footer" />
+                            <img src={twiter} className="social-icon-footer"/>
+                            <img src={instagram}className="social-icon-footer" />
+                        </div>
+                    </div>
 
                 </div>
             </footer>
 
-            <h1></h1>
         </div>
     )
 }
