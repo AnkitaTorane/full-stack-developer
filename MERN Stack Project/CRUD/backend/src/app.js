ac30618f7097d2a69eee160f49f94cde67f40e81
AnkitaTorane <ankitat5395@gmail.com>
2025-03-12 07:27:22 +0530

    Props

--- MERN Stack Project/CRUD/backend/src/app.js
+++ MERN Stack Project/CRUD/backend/src/app.js
@@ -56,3 +56,34 @@ app.post("/api/create-user", async (req, res) => { // Corrected missing forward 
         res.status(500).json({ message: "Internal Server Error", error });
     }
 });
+
+// Get  all users API (Get request) - retrieves all users from the database 
+app.get("/api/get-all-users", async (req, res) => {
+    try {
+        //Fetch all users from the database
+        const userslist = await User.find();
+
+        //send sucess responce with retireved users
+        res.status(200).json({ message: "users retireved successfully", data: users });
+
+    } catch (error) {
+        //Handle any errors and send an error responce 
+        res.status(500).json({ message: "Internal Server Error", error });
+
+
+    }
+});
+ app.delete("/api/delete-user/:id", async(req,res)=> {
+try {
+    const deleteUser = await User.findByIdAndDeleted(req.params.id)
+if(deleteUser){
+    res.status(200).json({message: "User deleted successfully"});
+}else{
+    res.status(404).json({message: "User not found"});
+}
+
+} catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error });
+}
+    
+ })
