94ee02f2a9ab08580f889f71e2713c1f21028250
AnkitaTorane <ankitat5395@gmail.com>
2025-04-08 23:18:25 +0530

    React_Router_Dom

---
+++ React.JS/interview prepration/routes/src/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+
+
+function About() {
+
+    const navi= useNavigate
+    return (
+    <div>
+      <h1>About page</h1>
+      <button type="button" className="btn btn-primary"onClick={}>Primary</button>
+    </div>
+    );
+  }
+  
+  export default About;
+  
---
+++ React.JS/interview prepration/routes/src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function contact() {
+    return (
+    <div>
+      <h1>About page</h1>
+      
+    </div>
+    );
+  }
+  
+  export default contact;
+  
